668e89e6460b4550cef7ee5c2807fbaba9fa097c
AdrianVader <adri.rabadan@gmail.com>
2016-08-31 18:22:43 +0200

    First commit
    First commit that includes a basic test scene and a basic animated
    player

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Constants
+{
+
+
+
+    // PLAYER MOVEMENT
+    public const string PLAYER_MOVE_LEFT = "a";
+    public const string PLAYER_MOVE_RIGHT = "d";
+    public const string PLAYER_JUMP = "space";
+
+    // PLAYER INTERACTION
+    public const string PLAYER_INTERACTION = "e";
+
+    // PLAYER ANIMATION
+    public const string PLAYER_ANIMATION_VARIABLE_SPEED = "Speed";
+    public const string PLAYER_ANIMATION_VARIABLE_JUMP = "Jump";
+    public const string PLAYER_ANIMATION_VARIABLE_GROUND = "Ground";
+
+    // PLAYER ANIMATION
+    public const float PLAYER_INITIAL_VALUE_LIFE = 100.0f;
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public string _behaviourName;
+    public float _life;
+
+	// Use this for initialization
+	void Start () {
+
+        this._behaviourName = null;
+        this._life = Constants.PLAYER_INITIAL_VALUE_LIFE;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // TODO: Interact (Constants.PLAYER_INTERACTION)
+        // Ask with a ray cast or mouse position if the object is interactive and use it if possible.
+
+	}
+}
