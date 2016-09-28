03adf70d0a53914a2107493a2a1838d58df01889
AdrianVader <adri.rabadan@gmail.com>
2016-09-28 02:04:40 +0200

    Scroll list to store inventory in the computer
    Scroll list to store inventory in the computer

--- Assets/Scripts/Computer/Computer.cs
+++ Assets/Scripts/Computer/Computer.cs
@@ -10,10 +10,18 @@ public class Computer : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+        start();
+
+	}
+
+    protected void start() {
+
         this._player = GameObject.FindGameObjectWithTag(Constants.PLAYER_TAG);
 
-        foreach (Transform child in this.gameObject.transform) {
-            if (child.name.Equals(Constants.COMPUTER_CANVAS_CHILD_NAME)) {
+        foreach (Transform child in this.gameObject.transform)
+        {
+            if (child.name.Equals(Constants.COMPUTER_CANVAS_CHILD_NAME))
+            {
                 foreach (Transform canvasChild in child.gameObject.transform)
                 {
                     if (canvasChild.name.Equals(Constants.COMPUTER_MAIN_MENU_CHILD_NAME))
@@ -24,28 +32,37 @@ public class Computer : MonoBehaviour {
             }
         }
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        // Hide computer's face
-        float distance = Mathf.Abs(Vector3.Distance(this._player.transform.position, this.gameObject.transform.position));
-
-        GameObject face = null;
-        foreach (Transform child in this.gameObject.transform)
-        {
-            if (child.name.Equals(Constants.COMPUTERS_FACE_CHILD_NAME))
-            {
-                face = child.gameObject;
-            }
-        }
-
-        if (distance > Constants.COMPUTER_MININUM_INTERACTION_DISTANCE) {
-            face.SetActive(false);
-            this._mainMenu.SetActive(false);
+        if (this._player == null) {
+            start();
         } else {
-            face.SetActive(true);
+
+            // Hide computer's face
+            float distance = Mathf.Abs(Vector3.Distance(this._player.transform.position, this.gameObject.transform.position));
+
+            GameObject face = null;
+            foreach (Transform child in this.gameObject.transform)
+            {
+                if (child.name.Equals(Constants.COMPUTERS_FACE_CHILD_NAME))
+                {
+                    face = child.gameObject;
+                }
+            }
+
+            if (distance > Constants.COMPUTER_MININUM_INTERACTION_DISTANCE)
+            {
+                face.SetActive(false);
+                this._mainMenu.SetActive(false);
+            }
+            else
+            {
+                face.SetActive(true);
+            }
+
         }
 
 	}
@@ -58,25 +75,29 @@ public class Computer : MonoBehaviour {
         
     }
 
-    void OnMouseOver()
-    {
+    void OnMouseOver() {
 
-        float distance = Mathf.Abs(Vector3.Distance(this._player.transform.position, this.gameObject.transform.position));
-        if (Input.GetKeyDown(Constants.PLAYER_USE_KEY) && distance <= Constants.COMPUTER_MININUM_INTERACTION_DISTANCE)
-        { // Use
+        if (this._player == null) {
+            start();
+        } else {
 
-            //List<GameObject> inventory = player.GetComponent<Player>()._inventory;
+            float distance = Mathf.Abs(Vector3.Distance(this._player.transform.position, this.gameObject.transform.position));
+            if (Input.GetKeyDown(Constants.PLAYER_USE_KEY) && distance <= Constants.COMPUTER_MININUM_INTERACTION_DISTANCE)
+            { // Use
 
-            // Create menu (GUI) with interaction options.
+                //List<GameObject> inventory = player.GetComponent<Player>()._inventory;
+
+                // Create menu (GUI) with interaction options.
                 // Tip -> Next possible steps to continue your adventure
                 // Storage -> Exchange items between your inventory and this device. Use some to change your skills.
                 // Research -> Invest your items and resources to create new awesome things or update them.
                 // Save -> Saves the game in the current state.
 
 
-            this._mainMenu.SetActive(true);
-            this._player.transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+                this._mainMenu.SetActive(true);
+                this._player.transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
 
+            }
         }
 
     }
